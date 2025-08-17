01b202499fff84780812e3958a9bf022293a322e
mi-shajib <mishajib222@gmail.com>
2025-08-17 22:13:57 +0600

    Conditional dependency injection by bean

--- src/main/java/com/mishajib/store/OrderService.java
+++ src/main/java/com/mishajib/store/OrderService.java
@@ -3,11 +3,11 @@ package com.mishajib.store;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
+    public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
