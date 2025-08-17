fa3f1e7deea8f5ac701d7a9b82a5d6bff2598873
mi-shajib <mishajib222@gmail.com>
2025-08-17 22:25:10 +0600

    Bean (IOC container) lifecycle hooks

--- src/main/java/com/mishajib/store/AppConfig.java
+++ src/main/java/com/mishajib/store/AppConfig.java
@@ -3,6 +3,7 @@ package com.mishajib.store;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
--- src/main/java/com/mishajib/store/OrderService.java
+++ src/main/java/com/mishajib/store/OrderService.java
@@ -1,5 +1,7 @@
 package com.mishajib.store;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,16 @@ public class OrderService {
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
         System.out.println("Order service created");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("OrderService PostConstruct");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("OrderService PreDestroy");
     }
 
     public void placeOrder() {
