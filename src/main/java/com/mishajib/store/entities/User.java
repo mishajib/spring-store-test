56fa8eb1c80fa6e2c9506aafb7970ec1702179b8
mi-shajib <mishajib222@gmail.com>
2025-08-18 20:27:21 +0600

    One-To-Many relationship

--- src/main/java/com/mishajib/store/entities/User.java
+++ src/main/java/com/mishajib/store/entities/User.java
@@ -38,4 +38,9 @@ public class User {
         addresses.add(address);
         address.setUser(this);
     }
+
+    public void removeAddress(Address address) {
+        addresses.remove(address);
+        address.setUser(null);
+    }
 }
