a2feac0a468fccb1baad1db32b5a81524842bc5b
mi-shajib <mishajib222@gmail.com>
2025-08-18 20:09:17 +0600

    With setter and constructor set the user data

--- src/main/java/com/mishajib/store/StoreApplication.java
+++ src/main/java/com/mishajib/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.mishajib.store;
 
+import com.mishajib.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,7 +9,11 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var user = new User(1L, "John Doe", "john@example.com", "john123");
+        user.setName("John Doe");
+        user.setEmail("john@example.com");
+        user.setPassword("john123");
     }
 
 }
