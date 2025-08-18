d466ea582d130bab13b87168be1572a885f6f94e
mi-shajib <mishajib222@gmail.com>
2025-08-18 19:41:39 +0600

    Added migration control by flyway

--- src/main/java/com/mishajib/store/StoreApplication.java
+++ src/main/java/com/mishajib/store/StoreApplication.java
@@ -9,9 +9,6 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var userService = context.getBean(UserService.class);
-        userService.registerUser(new User(1L, "test@example.com", "test", "test"));
-        userService.registerUser(new User(1L, "test@example.com", "test", "test"));
     }
 
 }
